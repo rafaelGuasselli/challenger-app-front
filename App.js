72cf7e2502a01cf938b03ec261bef5ba0a685440
rafaelguasselli <rafael.g.l.silva@gmail.com>
2025-09-18 19:27:54 -0300

    versão funcionando

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
+import 'react-native-url-polyfill/auto';
+import 'react-native-get-random-values';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello world! </Text>
     </View>
   );
 }
